e2776a0017449ee86e40864c1f9d0203110daaa1
ignasi <amargos.ignasi@gmail.com>
2021-03-08 14:29:02 +0100

    Added dependency packages

--- FRONTEND/best-friend-app/src/app/app.module.ts
+++ FRONTEND/best-friend-app/src/app/app.module.ts
@@ -4,16 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule
   ],
+  exports: [MatMenuModule],
   providers: [],
   bootstrap: [AppComponent]
 })
