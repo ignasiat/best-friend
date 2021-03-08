c978a4f2297c5e51dface35d444f4000bc76a12c
ignasi <amargos.ignasi@gmail.com>
2021-03-08 14:30:24 +0100

    Changes to app component to render header component

--- FRONTEND/best-friend-app/src/app/app.component.spec.ts
+++ FRONTEND/best-friend-app/src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('best-friend-app');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('best-friend-app app is running!');
-  });
 });
