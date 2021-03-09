619510419e5d50cd5c9fff45289596f67e9fe0b9
ignasi <amargos.ignasi@gmail.com>
2021-03-09 09:45:53 +0100

    Added Backend main file

---
+++ BACKEND/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const cors = require('cors');
+const { connect } = require('mongoose');
+
+const userRouter = require('./routers/userRouter');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+connect(process.env.DDBB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use('/api/user', userRouter);
+
+app.listen(port, () => debug(`Server running in ${chalk.green(port)}`));
