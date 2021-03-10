7f99c8cde372688064f60a432c870709f32bbc64
ignasi <amargos.ignasi@gmail.com>
2021-03-10 11:19:32 +0100

    Added file with constant values

---
+++ FRONTEND/best-friend-app/src/app/constants/index.ts
@@ -0,0 +1,29 @@
+interface constantsInterface {
+  FEMALE: String,
+  MALE: String,
+  SMALL: String,
+  MEDIUM: String,
+  LARGE: String,
+  XLARGE: String,
+  USER: String,
+  SHELTER: String,
+  PUPPY: String,
+  YOUNG: String,
+  ADULT: String,
+  SENIOR: String
+}
+
+export const constants: constantsInterface = {
+  FEMALE: 'FEMALE',
+  MALE: 'MALE',
+  SMALL: 'SMALL',
+  MEDIUM: 'MEDIUM',
+  LARGE: 'LARGE',
+  XLARGE: 'EXTRA LARGE',
+  USER: 'USER',
+  SHELTER: 'SHELTER',
+  PUPPY: 'PUPPY',
+  YOUNG: 'YOUNG',
+  ADULT: 'ADULT',
+  SENIOR: 'SENIOR'
+}
