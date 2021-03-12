546739b653798ace072c31b0bc3148b34edd5725
ignasi <amargos.ignasi@gmail.com>
2021-03-12 12:56:23 +0100

    Added methods to retrieve breeds and colors

--- FRONTEND/best-friend-app/src/app/core/services/dog-store.service.ts
+++ FRONTEND/best-friend-app/src/app/core/services/dog-store.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core'
 import { BehaviorSubject } from 'rxjs'
 import { Dog } from '../models/Dog'
 import { DogService } from './dog.service'
+import { constants } from '../../constants/index'
+import { Color } from '../models/Color'
+import { Breed } from '../models/Breed'
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +13,8 @@ export class DogStoreService {
   dogs$ = new BehaviorSubject<Dog[]>([])
   dogsCopy$ = new BehaviorSubject<Dog[]>([])
   selectedDog$ = new BehaviorSubject<Dog>(null)
+  breeds$ = new BehaviorSubject<Color[]>([])
+  colors$ = new BehaviorSubject<Breed[]>([])
 
   getDogs (): Dog[] {
     return this.dogs$.getValue()
@@ -21,6 +26,28 @@ export class DogStoreService {
       this.dogs$.next(answer)
       this.dogsCopy$.next(answer)
     })
+  }
+
+  apiBreeds (): void {
+    this.DogService.fetchBreeds().subscribe((answer) => this.breeds$.next(answer))
+  }
+
+  apiColors (): void {
+    this.DogService.fetchColors().subscribe((answer) => this.colors$.next(answer))
+  }
+
+  filteredDogs (sexValue: String, ageValue: String, sizeValue:String) {
+    let filtered: Dog[] = this.dogs$.getValue()
+    if (sexValue && sexValue !== constants.ANY) {
+      filtered = filtered.filter((element) => element.sex === sexValue)
+    }
+    if (ageValue && ageValue !== constants.ANY) {
+      filtered = filtered.filter((element) => element.age === ageValue)
+    }
+    if (sizeValue && sizeValue !== constants.ANY) {
+      filtered = filtered.filter((element) => element.size === sizeValue)
+    }
+    this.dogsCopy$.next(filtered)
   }
 
   getSelectedDog (dogId): void {
--- FRONTEND/best-friend-app/src/app/core/services/dog.service.ts
+++ FRONTEND/best-friend-app/src/app/core/services/dog.service.ts
@@ -2,12 +2,14 @@ import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
 import { Dog } from '../models/Dog'
+import { Color } from '../models/Color'
+import { Breed } from '../models/Breed'
 
 @Injectable({
   providedIn: 'root'
 })
 export class DogService {
-  dogsApi = 'http://localhost:5000/api/dog'
+  dogsApi = 'http://localhost:5000/api'
 
   constructor (
     private httpClient: HttpClient
@@ -16,6 +18,14 @@ export class DogService {
   }
 
   fetchDogs (): Observable<Dog[]> {
-    return this.httpClient.get<Dog[]>(`${this.dogsApi}`)
+    return this.httpClient.get<Dog[]>(`${this.dogsApi}/dog`)
+  }
+
+  fetchBreeds (): Observable<Breed[]> {
+    return this.httpClient.get<Breed[]>(`${this.dogsApi}/breed`)
+  }
+
+  fetchColors (): Observable<Color[]> {
+    return this.httpClient.get<Color[]>(`${this.dogsApi}/color`)
   }
 }
