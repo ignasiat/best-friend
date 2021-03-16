738ccada5ccc963dadd7618c3730a719243c058f
ignasi <amargos.ignasi@gmail.com>
2021-03-16 17:57:53 +0100

    Update UserComponent to work

--- FRONTEND/best-friend-app/src/app/pages/user/user.component.ts
+++ FRONTEND/best-friend-app/src/app/pages/user/user.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
+import { Observable } from 'rxjs'
+import { Dog } from 'src/app/core/models/Dog'
+import { DogStoreService } from 'src/app/core/services/dog-store.service'
 
 @Component({
   selector: 'app-user',
@@ -9,8 +12,13 @@ import { ActivatedRoute } from '@angular/router'
 export class UserComponent implements OnInit {
   id = this.activatedRoute.snapshot.params.userId
 
-  constructor (private activatedRoute: ActivatedRoute) { }
+  constructor (private activatedRoute: ActivatedRoute, private DogStoreService: DogStoreService) { }
+
+  selectedshelter: any
+  ourDogs$: Observable<Dog[]>
 
   ngOnInit (): void {
+    this.DogStoreService.apiShelter().subscribe((shelters) => { this.selectedshelter = shelters.find((shelter) => shelter._id === this.id) })
+    this.ourDogs$ = this.DogStoreService.apiDogsUser(this.id)
   }
 }
