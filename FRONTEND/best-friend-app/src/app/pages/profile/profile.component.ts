83ed0aeaac120c88f7e9cd16c25e4328f9f40481
ignasi <amargos.ignasi@gmail.com>
2021-03-19 17:21:16 +0100

    Profile Component implementation with functionalities

--- FRONTEND/best-friend-app/src/app/pages/profile/profile.component.ts
+++ FRONTEND/best-friend-app/src/app/pages/profile/profile.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
 import { DogStoreService } from 'src/app/core/services/dog-store.service'
-import { filter, switchMap, tap } from 'rxjs/operators'
-import { BehaviorSubject, Observable } from 'rxjs'
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap'
+import { PopupdeleteComponent } from './popupdelete/popupdelete.component'
+import { BehaviorSubject } from 'rxjs'
+import { Dog } from 'src/app/core/models/Dog'
+import { tap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-profile',
@@ -13,25 +16,65 @@ import { BehaviorSubject, Observable } from 'rxjs'
 export class ProfileComponent implements OnInit {
   constructor (
     private DogStoreService: DogStoreService,
-    private router: Router
+    private router: Router,
+    private modalService: NgbModal
   ) {}
 
-  ourDogs$ = new Observable();
+  dogs$: BehaviorSubject<Dog[]> = this.DogStoreService.dogs$
+  dogsUser$: BehaviorSubject<Dog[]> = this.DogStoreService.dogsUser$
+  dogsUserForAdoption: Dog[]
+  dogsUserAdopted: Dog[]
+  dogsOwner: Dog[]
   isLogged$ = new BehaviorSubject(null);
 
   userLogged$ = this.DogStoreService.userLogged$
     .pipe(
       tap(isLogged => !isLogged && this.router.navigate(['/'])),
       tap(user => {
-        if (user) this.isLogged$.next(user._id)
+        if (user) {
+          this.DogStoreService.filterUserDogs(user._id)
+          this.isLogged$.next(user)
+          this.filter(user._id)
+        }
       })
     )
 
   ngOnInit () {
-    this.ourDogs$ = this.isLogged$
-      .pipe(
-        filter(event => !!event),
-        switchMap((id: string) => this.DogStoreService.apiDogsUser(id))
-      )
+    if (!this.dogs$.getValue().length) {
+      this.DogStoreService.apiDogsAdoption().subscribe(() => {
+        const user = this.isLogged$.getValue()
+        if (user) {
+          this.DogStoreService.filterUserDogs(user._id)
+        }
+      })
+    }
+  }
+
+  delete (dogId: string) {
+    const modalRef = this.modalService.open(PopupdeleteComponent)
+    modalRef.componentInstance.dogId = dogId
+    modalRef.dismissed.subscribe(() => {
+      this.filter(this.isLogged$.getValue()._id)
+      this.DogStoreService.filterAdoptionDogs()
+    })
+  }
+
+  filter (userId: string): void {
+    this.dogsUserForAdoption = this.dogsUser$.getValue().filter((dog) => dog.adoption)
+    this.dogsUserAdopted = this.dogsUser$.getValue().filter((dog) => !dog.adoption)
+    this.dogsOwner = this.dogs$.getValue().filter((dog) => dog.owner?._id === userId)
+  }
+
+  adoption (dog: Dog, adoption: boolean) {
+    const updatedDog = { ...dog, adoption, owner: null }
+    const dogId = dog._id
+    delete updatedDog._id
+    this.DogStoreService.updateDogApi(dogId, updatedDog).subscribe(() => {
+      const user = this.isLogged$.getValue()
+      if (user) {
+        this.DogStoreService.filterUserDogs(user._id)
+        this.filter(user._id)
+      }
+    })
   }
 }
