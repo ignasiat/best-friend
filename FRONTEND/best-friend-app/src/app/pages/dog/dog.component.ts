2e7453808d9acc91f45f65a9e99100c878d28ca8
ignasi <amargos.ignasi@gmail.com>
2021-03-19 17:28:18 +0100

    Added Adopt functionality to DogComponent

--- FRONTEND/best-friend-app/src/app/pages/dog/dog.component.ts
+++ FRONTEND/best-friend-app/src/app/pages/dog/dog.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
 import { DogStoreService } from '../../core/services/dog-store.service'
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap'
+import { User } from 'src/app/core/models/User'
+import { Dog } from 'src/app/core/models/Dog'
 
 @Component({
   selector: 'app-dog',
@@ -12,10 +14,12 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap'
 export class DogComponent implements OnInit {
   id = this.activatedRoute.snapshot.params.dogId
   selectedDog$ = this.DogStoreService.selectedDog$
+  userLogged: User
 
   constructor (
   private activatedRoute: ActivatedRoute,
   private DogStoreService: DogStoreService,
+  private router: Router,
   config: NgbCarouselConfig
   ) {
     config.interval = 2000
@@ -25,5 +29,13 @@ export class DogComponent implements OnInit {
 
   ngOnInit (): void {
     this.DogStoreService.getSelectedDog(this.id)
+    this.userLogged = this.DogStoreService.userLogged$.getValue()
+  }
+
+  adopt (dog: Dog, userId: string) {
+    const newData = { ...dog, adoption: false, owner: { _id: userId } }
+    const dogId = dog._id
+    delete newData._id
+    this.DogStoreService.updateDogApi(dogId, newData).subscribe(() => this.router.navigate(['/profile']))
   }
 }
