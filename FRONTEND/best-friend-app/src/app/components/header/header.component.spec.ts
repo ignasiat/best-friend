ca43482ae65753987f82d4fc675be9ad6c55ed8f
ignasi <amargos.ignasi@gmail.com>
2021-03-24 12:51:50 +0100

    Refactor Header component and add tests

--- FRONTEND/best-friend-app/src/app/components/header/header.component.spec.ts
+++ FRONTEND/best-friend-app/src/app/components/header/header.component.spec.ts
@@ -1,16 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { MatMenuModule } from '@angular/material/menu'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { RouterTestingModule } from '@angular/router/testing'
 
 import { HeaderComponent } from './header.component'
+import { Router } from '@angular/router'
+import { userMock } from 'src/app/constants/user-mock'
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent
   let fixture: ComponentFixture<HeaderComponent>
+  let router: Router
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HeaderComponent],
-      imports: [MatMenuModule]
+      imports: [
+        MatMenuModule,
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([])]
     })
       .compileComponents()
   })
@@ -19,9 +27,40 @@ describe('HeaderComponent', () => {
     fixture = TestBed.createComponent(HeaderComponent)
     component = fixture.componentInstance
     fixture.detectChanges()
+    router = TestBed.inject(Router)
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('Should navigate to "/" when logout', () => {
+    spyOn(component.DogStore, 'apiSignOut').and.returnValue(null)
+
+    const navigateSpy = spyOn(router, 'navigate')
+
+    component.logout()
+
+    expect(navigateSpy).toHaveBeenCalledOnceWith(['/'])
+  })
+
+  it('Should navigate to "/user/userId" when userLogged is truthy and mobilemenu is invoked', () => {
+    component.userLogged = userMock
+
+    const navigateSpy = spyOn(router, 'navigate')
+
+    component.mobileMenu()
+
+    expect(navigateSpy).toHaveBeenCalledOnceWith(['/user', 'userId'])
+  })
+
+  it('Should navigate to "/signin" when userLogged is falsy and mobilemenu is invoked', () => {
+    component.userLogged = null
+
+    const navigateSpy = spyOn(router, 'navigate')
+
+    component.mobileMenu()
+
+    expect(navigateSpy).toHaveBeenCalledOnceWith(['/signin'])
   })
 })
