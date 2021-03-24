7c191d6e7dd14754fd2a8545f2f78372c6a227bb
ignasi <amargos.ignasi@gmail.com>
2021-03-24 14:30:25 +0100

    Refactor PopUpComponent make Modal private and add tests

--- FRONTEND/best-friend-app/src/app/pages/sign-in/popup/popup.component.spec.ts
+++ FRONTEND/best-friend-app/src/app/pages/sign-in/popup/popup.component.spec.ts
@@ -1,25 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { PopupComponent } from './popup.component';
+import { PopupComponent } from './popup.component'
+
+import { NgbModal, NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 describe('PopupComponent', () => {
-  let component: PopupComponent;
-  let fixture: ComponentFixture<PopupComponent>;
+  let component: PopupComponent
+  let fixture: ComponentFixture<PopupComponent>
+  let modal: NgbModal
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PopupComponent ]
+      imports: [NgbModalModule, NgbModule],
+      declarations: [PopupComponent]
     })
-    .compileComponents();
-  });
+      .compileComponents()
+  })
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PopupComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(PopupComponent)
+    modal = TestBed.inject(NgbModal)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+
+  it('Should call dismissAll when close is invoked', () => {
+    const nextSpyOn = spyOn(modal, 'dismissAll')
+
+    component.close('close')
+
+    expect(nextSpyOn).toHaveBeenCalled()
+  })
+})
