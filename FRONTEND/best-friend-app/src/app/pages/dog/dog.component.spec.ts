640f1931c0d9705eb1112c4d1406a5e3b4a25c8f
ignasi <amargos.ignasi@gmail.com>
2021-03-24 13:03:22 +0100

    Rename Store in component, fix styles and add tests

--- FRONTEND/best-friend-app/src/app/pages/dog/dog.component.spec.ts
+++ FRONTEND/best-friend-app/src/app/pages/dog/dog.component.spec.ts
@@ -1,12 +1,16 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { Router } from '@angular/router'
 import { RouterTestingModule } from '@angular/router/testing'
+import { of } from 'rxjs'
+import { dogMock } from 'src/app/constants/dog-mock'
 
 import { DogComponent } from './dog.component'
 
 describe('DogComponent', () => {
   let component: DogComponent
   let fixture: ComponentFixture<DogComponent>
+  let router: Router
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,9 +23,20 @@ describe('DogComponent', () => {
     fixture = TestBed.createComponent(DogComponent)
     component = fixture.componentInstance
     fixture.detectChanges()
+    router = TestBed.inject(Router)
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('Should navigate to "/profile" when adopt is invoked', () => {
+    spyOn(component.DogStore, 'updateDogApi').and.returnValue(of(null))
+
+    const navigateSpy = spyOn(router, 'navigate')
+
+    component.adopt(dogMock, 'userId')
+
+    expect(navigateSpy).toHaveBeenCalledOnceWith(['/profile'])
   })
 })
