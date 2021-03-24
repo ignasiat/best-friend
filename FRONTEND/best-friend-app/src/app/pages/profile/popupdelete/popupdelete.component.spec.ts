7bca7ead9399389b12e5ea47f6e0cace411f2b2f
ignasi <amargos.ignasi@gmail.com>
2021-03-24 13:23:50 +0100

    Refactor PopUpDeleteComponent and add test

--- FRONTEND/best-friend-app/src/app/pages/profile/popupdelete/popupdelete.component.spec.ts
+++ FRONTEND/best-friend-app/src/app/pages/profile/popupdelete/popupdelete.component.spec.ts
@@ -1,25 +1,60 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { PopupdeleteComponent } from './popupdelete.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { RouterTestingModule } from '@angular/router/testing'
+import { NgbModal, NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { PopupdeleteComponent } from './popupdelete.component'
+import { DogStoreService } from 'src/app/core/services/dog-store.service'
+import { dogMock } from 'src/app/constants/dog-mock'
+import { of } from 'rxjs'
+import { userMock } from 'src/app/constants/user-mock'
 
 describe('PopupdeleteComponent', () => {
-  let component: PopupdeleteComponent;
-  let fixture: ComponentFixture<PopupdeleteComponent>;
+  let component: PopupdeleteComponent
+  let fixture: ComponentFixture<PopupdeleteComponent>
+  let modal: NgbModal
+  let dogStore: DogStoreService
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PopupdeleteComponent ]
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        NgbModalModule,
+        NgbModule],
+      declarations: [PopupdeleteComponent]
     })
-    .compileComponents();
-  });
+      .compileComponents()
+  })
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PopupdeleteComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(PopupdeleteComponent)
+    modal = TestBed.inject(NgbModal)
+    dogStore = TestBed.inject(DogStoreService)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+
+  it('should invoke modal method dismissAll when close is invoked', () => {
+    const nextSpyOn = spyOn(modal, 'dismissAll')
+
+    component.close()
+
+    expect(nextSpyOn).toHaveBeenCalled()
+  })
+
+  it('should invoke modal method dismissAll when delete is invoked', () => {
+    const nextSpyOn = spyOn(modal, 'dismissAll')
+
+    spyOn(dogStore, 'eraseDogApi').and.returnValue(of(dogMock))
+
+    component.userLogged$.next(userMock)
+
+    component.delete()
+
+    expect(nextSpyOn).toHaveBeenCalled()
+  })
+})
